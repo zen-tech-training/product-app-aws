a3d2e58e75f2bcccb189cbdb63472d4ec0503c2b
zen-tech-training <117732986+zen-tech-training@users.noreply.github.com>
2024-12-09 05:32:49 +0530

    Pen added

--- src/main/java/com/product/controller/ProductController.java
+++ src/main/java/com/product/controller/ProductController.java
@@ -28,6 +28,7 @@ public class ProductController {
 		productList.add(new Product(++productIdCounter, "Chairs", 30, 1500));
 		productList.add(new Product(++productIdCounter, "Tables", 15, 6000));
 		productList.add(new Product(++productIdCounter, "Laptop", 3, 80000));
+		productList.add(new Product(++productIdCounter, "Pen", 20, 70));
 	}
 	
 	@GetMapping("/product")
